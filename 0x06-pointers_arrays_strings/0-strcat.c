28d655b2af93ebea6d73d9b064e0c42f3509504d
codeafrik <acheampongf94@gmail.com>
2022-03-24 12:58:10 +0000

    more pointers and arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,30 @@
 #include "main.h"
 
 /**
-* *_strcat - function that concatenates two strings
-* @dest: character pointer variable
-* @src: character pointer variable
-* Return: pointer to the result sting dest
-*/
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
 char *_strcat(char *dest, char *src)
 {
-int i, j = 0;
+	int i, j;
 
-for (i = 0;dest[i] != '\0'; i++)
-;
-while (src[j] != '\0')
-{
-dest[i + j] = src[j];
-j++;
-}
+	i = 0;
+	j = 0;
 
-return (dest);
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,34 @@
 #include "main.h"
 
 /**
-* cap_string - function that capitalizes all words of a strin
-* @ch: pointer to input string
-* Return: pointer to Capitalized string
-*/
-char *cap_string(char *ch)
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
 {
-int i, j;
+	int i, j;
 
-char sep[] = " \t\n,;.!?\"(){}";
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
-if (ch[0] == 'a' && ch[0] == 'z')
-ch[0] = 'A' + (ch[0] - 'a');
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 
-i = 1;
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
 
-while (ch[i] != '\0')
-{
-for (j = 0; sep[j] != '\0'; j++)
-if (ch[i - 1] == sep[j] && (ch[i] >= 'a' && ch[i] <= 'z'))
-ch[i] = 'A' + (ch[i] - 'a');
-i++;
-}
-return (ch);
+	return (s);
 }
